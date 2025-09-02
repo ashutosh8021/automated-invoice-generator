a7a900038bcecad58de8b249f34e0110ea225af8
ashutosh8021 <ashutosh_2312res778@iitp.ac.in>
2025-09-03 02:30:46 +0530

    SECURITY

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://ysdbashzhpwjoidmvbmr.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzZGJhc2h6aHB3am9pZG12Ym1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4NDE3MjUsImV4cCI6MjA3MjQxNzcyNX0.-wK76ucgIAkHQECSz3cHTRPwjpoWvRQuBx1vkCVYn3M',
-  apiUrl: 'https://ysdbashzhpwjoidmvbmr.supabase.co/rest/v1'
-};
